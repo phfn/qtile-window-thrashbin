49ec63727589fed9b62c15eae79581c4d45ab6a5
Paul Hoffmann <phfn@phfn.de>
2021-08-07 23:02:28 +0200

    Add License

--- src/trashbin.py
+++ src/trashbin.py
@@ -1,5 +1,22 @@
+# Copyright (C) 2021 Paul Hoffmann <phfn@phfn.de
+
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 "A thrashbin for qtile windows."
+
 __version__ = "0.0.1"
+
 from libqtile.core.manager import Qtile
 from libqtile.backend.base import Window
 from libqtile import qtile
